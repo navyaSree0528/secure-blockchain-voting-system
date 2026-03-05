55b5c4278f217aa7a59fc22e9ae3abf605d2c162
Navya <abc@gamil.com>
2026-03-05 20:09:30 +0530

    Updated encryption logic and project files

--- crypto/encryption.py
+++ crypto/encryption.py
@@ -1,16 +1,16 @@
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
+# Fixed encryption key (generate once and keep it same)
+key = b'5PzX7Q1oY9mR3xXhBv3cP0L2k8qTjG5V9hZxT4wF6sA='
+
 cipher = Fernet(key)
 
 
 def encrypt_vote(vote):
-
-    encrypted = cipher.encrypt(vote.encode())
-    return encrypted
+    encrypted_vote = cipher.encrypt(vote.encode())
+    return encrypted_vote
 
 
 def decrypt_vote(encrypted_vote):
-
     vote = cipher.decrypt(encrypted_vote).decode()
     return vote
